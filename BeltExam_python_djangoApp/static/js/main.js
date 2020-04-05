389479ecb37adaf65912a644d552edc222c89507
Olanrewaju Oyewo <oyewobenjamin@yahoo.com>
2020-04-05 17:04:56 -0400

    final demo version

---
+++ BeltExam_python_djangoApp/static/js/main.js
@@ -0,0 +1,16 @@
+// IMAGE EFFECTS BELOW
+wow = new WOW(
+    {
+      animateClass: 'animated',
+      offset:       100,
+      callback:     function(box) {
+        console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+      }
+    }
+  );
+  wow.init();
+  document.getElementById('moar').onclick = function() {
+    var section = document.createElement('section');
+    section.className = 'section--purple wow fadeInDown';
+    this.parentNode.insertBefore(section, this);
+  };
